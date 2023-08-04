cb2aea27a9bb6c56940568e097441da603571a33
issac2001 <83367715+issac2001@users.noreply.github.com>
2023-08-05 04:44:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 
-const API_URL = 'http://www.omdbapi.com?apikey=f19c2892';
+const API_URL = 'https://www.omdbapi.com?apikey=f19c2892';
 const App =  () => {
    const [movies,setMovies] = useState([]);
    const [searchTerm, setSearchTerm] = useState('');
